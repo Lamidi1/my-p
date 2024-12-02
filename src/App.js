7d0a890e06e3eff331b3970c5310acc471ba970d
lamidi1 <labassgbolahan@gmail.com>
2024-12-02 13:23:43 -0800

    recreated the footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
- import Home from "./components/Home";
- import Header from "./components/Header";
- import About from "./components/About";
- import Projects from "./components/Projects";
- import Skills from "./components/Skills";
- import Contact from "./components/Contact";
- import Footer from "./components/Footer";
-  // import Loading from "./components/Loading";
-  // import { useState,useEffect } from "react";
-  
-  import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./components/Home";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
+// import Loading from "./components/Loading";
+// import { useState,useEffect } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   // const [isLoading, setIsLoading] = useState(true);
@@ -17,33 +18,29 @@ function App() {
   //    setTimeout(()=>{
   //     setIsLoading(false);
   //    },5000)
-  
+
   // },)
 
   return (
-    
     <BrowserRouter>
-<Footer/>
-    <div >
-      
+      <Footer />
+
+      <div>
         {/* {isLoading===true?
           <Loading/>:
           <Home/>
         }
          */}
-    
-    
-    <Routes>
-    <Route  path='/'element={<Home/>}/>
-    <Route  path='/header'element={<Header/>}/>
-    <Route  path='/about'element={<About/>}/>
-    <Route  path='/projects'element={<Projects/>}/>
-    <Route  path='/skills'element={<Skills/>}/>
-    <Route  path='/contact'element={<Contact/>}/>
-    </Routes>
-    
-    </div>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/header" element={<Header />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
